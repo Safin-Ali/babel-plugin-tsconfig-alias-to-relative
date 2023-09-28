a9d2c55edfdfc1ad2ac524135a18ea9c8ea3c771
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-28 23:43:39 +0600

    changes to path alias to the relative path

--- src/utility/path-aliases.js
+++ src/utility/path-aliases.js
@@ -1,21 +1,49 @@
-const {parse, resolve, basename,dirname} = require('node:path');
+const { parse,sep } = require('node:path');
 
-const jsonParse = require('json-to-js-obj');
+const { ts_alias, rootDir } = require('./tsconf-path-alias');
 
 const logger = require('./color-logger');
 
-const rootDir = resolve(basename(__dirname),'..');
-
 module.exports = function () {
-  return {
-	visitor: {
-		ImportDeclaration(path,state) {
-			const rawval = path.node.source.value;
-			const valState = state.filename;
-			logger.process(`start`);
-			console.log(valState,rawval);
-			logger.error(`end`);
-		}}
-  };
+	const result = {
+		visitor: {
+			ImportDeclaration(path, state) {
+				const rawVal = path.node.source.value;
+				const valState = state.filename;
+				let fixedAlias = false;
+				if (!rawVal.startsWith('@')) return;
+				const { dir } = parse(valState);
+				const firstSegAlias = rawVal.split('/');
+				let tsAlias = ts_alias[firstSegAlias[0] + "/*"];
+				const aliasLevel = dir.replace(rootDir + '\\', '').split(sep);
+				if (!tsAlias) {
+					tsAlias = ts_alias[firstSegAlias[0]];
+					fixedAlias = true;
+				}
+				tsAlias.forEach(als => {
+					let actualPath = '';
+					aliasLevel.forEach(() => {
+						actualPath = actualPath.concat('../');
+					});
+					if (fixedAlias) {
+						const rmvSymbol = firstSegAlias.slice(-1)[0].replace(/^[~@]/, "")
+						als = actualPath.concat(als.replace(/\/[^\/]+\.ts$/, "/" + rmvSymbol));
+						fixedAlias = false;
+					} else {
+						als = actualPath.concat(als.replace(/\/[^\/]+\.ts$/, "/" + firstSegAlias.slice(-1)[0]));
+					}
+
+					if (als.split('../').length <= 2) {
+						als = als.replace('../src', ".");
+					} else {
+						als = als.replace('../src/', "");
+					}
+					path.node.source.value = als;
+				});
+			}
+		}
+	};
+	logger.success('tsconfig path alias resolved successfully')
+	return result;
 };
 
