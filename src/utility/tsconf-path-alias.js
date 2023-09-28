df99fe791521ce6ee261ee08f051742193614ad0
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-29 01:01:59 +0600

    added comments for tsconfig handler module

--- src/utility/tsconf-path-alias.js
+++ src/utility/tsconf-path-alias.js
@@ -1,9 +1,26 @@
+/**
+ * Module for handle `tsconfig,json`, `print status with meaningful color`.
+ * @module tsConfFile
+ */
+
 const jsonParse = require('json-to-js-obj');
 const { resolve, basename, } = require('node:path');
 const { readFileSync } = require('node:fs');
 const logger = require('./color-logger');
 
+/**
+ * The root directory path.
+ * @type {string}
+ */
 const rootDir = resolve(basename(__dirname), '..');
+
+/**
+ * Try to read `tsconfig.json`.
+ * @function - This is a `IIFE` function.
+ * @returns {String} - The JSON will return as `string`
+ * @throws {Error} - Throws an error if tsconfig.json is missing.
+ */
+
 const tsConfFile = (() => {
 	try {
 		return readFileSync(resolve(rootDir, 'tsconfig.json'), 'utf-8')
@@ -14,6 +31,11 @@ const tsConfFile = (() => {
 	}
 })();
 
+/**
+ * The TypeScript aliases defined in the tsconfig.json file.
+ * @type {Object}
+ * @throws {Error} Throws an error if parsing or required properties are missing.
+ */
 const ts_alias = (() => {
 	try {
 		return jsonParse(tsConfFile).compilerOptions.paths
