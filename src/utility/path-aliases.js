51059a54dcd455792b7f1bbbf2fccf3b4fc5aff6
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-29 14:27:30 +0600

    check alias are exist to tsconfig.json and support only src root dir

--- src/utility/path-aliases.js
+++ src/utility/path-aliases.js
@@ -4,6 +4,23 @@ const { ts_alias, rootDir } = require('./tsconf-path-alias');
 
 const logger = require('./color-logger');
 
+const symbolSTReg = /^[~@]/;
+
+/**
+ *
+ * @param {String} alias - The alias to be checked.
+ * @param {Object} tsAlias - he TypeScript alias object to be searched.
+ * @returns {boolean} Returns `true` if the alias exists in the tsAlias object, otherwise `false`.
+ */
+
+const aliasExist = (alias,tsAlias) => {
+	const symbolCheck = symbolSTReg.test(alias);
+	const aliasOnly = alias.split('/')[0];
+	if(!symbolCheck) return false;
+	if(tsAlias.hasOwnProperty(aliasOnly+'/*') || tsAlias.hasOwnProperty(aliasOnly)) return true;
+	return false;
+};
+
 /**
  * Resolves TypeScript path aliases in import declarations.
  * @function
@@ -21,8 +38,8 @@ module.exports = function () {
 			ImportDeclaration(path, state) {
 				const rawVal = path.node.source.value;
 				const valState = state.filename;
+				if(!aliasExist(rawVal,ts_alias)) return;
 				let fixedAlias = false;
-				if (!rawVal.startsWith('@')) return;
 				const { dir } = parse(valState);
 				const firstSegAlias = rawVal.split('/');
 				let tsAlias = ts_alias[firstSegAlias[0] + "/*"];
@@ -37,7 +54,7 @@ module.exports = function () {
 						actualPath = actualPath.concat('../');
 					});
 					if (fixedAlias) {
-						const rmvSymbol = firstSegAlias.slice(-1)[0].replace(/^[~@]/, "")
+						const rmvSymbol = firstSegAlias.slice(-1)[0].replace(symbolSTReg, "")
 						als = actualPath.concat(als.replace(/\/[^\/]+\.ts$/, "/" + rmvSymbol));
 						fixedAlias = false;
 					} else {
@@ -45,10 +62,11 @@ module.exports = function () {
 					}
 
 					if (als.split('../').length <= 2) {
-						als = als.replace('../', ".");
+						als = als.replace('../', "./");
+						als = als.replace(".ts",".js");
 					} else {
-						als = als.replace(`/`,"")
 						als = als.replace('../', "");
+						als = als.replace(".ts",".js");
 					}
 					path.node.source.value = als;
 				});
